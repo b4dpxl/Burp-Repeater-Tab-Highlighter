1550ace49ce15f4d55ce3490f87306b63676c562
b4dpxl <b4d@pxl.me.uk>
2020-11-26 17:39:17 +0000

    Added persistence and more docs

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # Burp Repeater Tab Highlighter
 
-Adds a context menu to a Burp Repeater tab to change the tab colour. Not persistent. 
-Right-click the request/response body, not the tab itself!
+This [Burp Suite](https://portswigger.net/burp) extension allows you to highlight Repeater tabs 
+in different colours and fonts.
+
+![](screens/tabs.png)
+
+It adds a context menu to a Repeater tab to change the tab's label colour. Right-click the 
+request/response body, not the tab itself!
+
+![](screens/menu.png)
+
+The colours are stored in a new entry on the site map, http://tabhighlighterextension.local/state. 
+This is updated when the plugin unloads (either shutting down Burp or reloading the extension), or 
+manually via the "Save now" menu option. It is not saved on every change to try and be more 
+efficient. This may change in the future.
+
+However, this means that if Burp crashes you might lose settings, or things might get confused if 
+you've inserted (not added to the end) or moved Repeater tabs. Additionally, if you insert/move 
+tabs without the extension running things might get confused. The states are just stored in an 
+array by the tab position.
--- tab_highlighter.py
+++ tab_highlighter.py
@@ -4,21 +4,23 @@ Change the colour of selected tabs in Burp Repeater. Useful when you have lots o
 History:
 0.0.1: First vresion
 0.1.0: Remembers tab setting when re-ordering tabs
+1.0.0: Colours are stored in a new item on the Site Map (on unload) to last between restarts of Burp
 """
 __author__ = "b4dpxl"
 __license__ = "GPL"
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
-import re
+import json
 import sys
 import traceback
 
-from burp import IBurpExtender, IContextMenuFactory, IExtensionStateListener
+from burp import IBurpExtender, IContextMenuFactory, IExtensionStateListener, IHttpService, IHttpRequestResponse
 
 # Java imports
 from javax import swing
 from java.util import List, ArrayList
 from java.awt import Color, Frame, Font
+from urlparse import urlparse
 
 
 NAME = "Tab Highlighter"
@@ -43,12 +45,10 @@ class BurpExtender(IBurpExtender, IExtensionStateListener, IContextMenuFactory):
 
     _tabs = {}
 
-    """
-    Note: For some reason we have to use TabbedPane.setBackgroundAt() to set the colour,
-    but (Tab Component).getForeground() to retrieve it :shrug:
+    CONFIG_URL = 'http://tabhighlighterextension.local/state'
 
-    TODO: try and make this persistent between Burp restarts
-    """
+    # Note: For some reason we have to use TabbedPane.setBackgroundAt() to set the colour,
+    # but (Tab Component).getForeground() to retrieve it :shrug:
 
     def registerExtenderCallbacks(self, callbacks):
         # for error handling
@@ -69,27 +69,49 @@ class BurpExtender(IBurpExtender, IExtensionStateListener, IContextMenuFactory):
         callbacks.registerExtensionStateListener(self)
         callbacks.registerContextMenuFactory(self)
         self._repeater.addChangeListener(self.tabChanged)
-        # preload tab colours for an extension reload
+        # load saved tab colours
+        colours = self.loadSettings()
+        if colours:
+            for idx, col in enumerate(colours):
+                if col:  # un-highlighted tabs are empty arrays
+                    self._highlight_tab(None, Color(col[0], col[1], col[2]), col[3], idx=idx)
+
+    def saveSettings(self, event=None):
+        print("Saving colours")
+        settings = []
+        newTabTab = self._repeater.getTabComponentAt(self._repeater.getTabCount()-1)
+        baseColor = newTabTab.getComponent(0).getForeground()
         for idx in range(self._repeater.getTabCount()-1):
             tab = self._repeater.getTabComponentAt(idx)
             tabLabel = tab.getComponent(0)
-            self._tabs[tab] = [
-                tabLabel.getForeground(), 
-                tabLabel.getFont().getStyle()
-            ]
+            tabColour = tabLabel.getForeground()
+            if tabColour == baseColor:  # not hightlighted, ignore it. This should handle theme changes
+                settings.append([])
+            else:
+                settings.append([
+                    tabColour.getRed(), tabColour.getGreen(), tabColour.getBlue(), tabLabel.getFont().getStyle()
+                ])
+        self._callbacks.addToSiteMap(ConfigStoreRequestResponse(self.CONFIG_URL, json.dumps(settings)))
+
+    def loadSettings(self):
+        requestResponse = self._callbacks.getSiteMap(self.CONFIG_URL)
+        if requestResponse and requestResponse[0].getResponse():
+            print("Loading colours")
+            resp = self._helpers.analyzeResponse(requestResponse[0].getResponse())
+            body = self._helpers.bytesToString(requestResponse[0].getResponse()[resp.getBodyOffset():]).encode('ascii', 'ignore')
+            return json.loads(body)
 
     def tabChanged(self, event):
         idx = self._repeater.getSelectedIndex()
         tab = self._repeater.getTabComponentAt(idx)
         tabLabel = tab.getComponent(0)
-        if self._tabs.get(tab):  # known tab, set the colour/style in case it was moved. TODO can we detect movement?
+        if self._tabs.get(tab):  
             self._highlight_tab(None, *self._tabs.get(tab))
-        # else:  # unknown tab. Should only occur for un-highlighted tabs?
-            # self._tabs[tab] = [tabLabel.getForeground(), tabLabel.getFont().getStyle()]
 
     def extensionUnloaded(self):
         self._repeater.removeChangeListener(self.tabChanged)
-        print("unloaded " + NAME)
+        self.saveSettings()
+        print("Unloaded " + NAME)
 
     def _find_repeater(self, container):
         if container.getComponents() and self._repeater is None:
@@ -98,7 +120,6 @@ class BurpExtender(IBurpExtender, IExtensionStateListener, IContextMenuFactory):
                     if c.getTabCount > 0:
                         for x in range(c.getTabCount()):
                             if c.getTitleAt(x) == "Repeater":
-                                # print("Got Repeater tab")
                                 self._repeater = c.getComponentAt(x)
                                 return
                 except:
@@ -107,7 +128,7 @@ class BurpExtender(IBurpExtender, IExtensionStateListener, IContextMenuFactory):
 
     def _createItemStyled(self, text, colour, style):
         item = swing.JMenuItem(text, actionPerformed=lambda x: self._highlight_tab(x, colour, style))
-        item.setFont(Font(item.getFont().getName(), style, item.getFont().getSize()))
+        item.setFont(item.getFont().deriveFont(style))
         return item
 
     def _createItem(self, name, colour):
@@ -127,23 +148,108 @@ class BurpExtender(IBurpExtender, IExtensionStateListener, IContextMenuFactory):
 
         menu = ArrayList()
         subMenu = swing.JMenu("Highlight Tab")
-        subMenu.setForeground(Color(255, 204, 51))
+        # subMenu.setForeground(Color(255, 204, 51))  # uncomment this line if you want the menu item to be highlighted itself
         subMenu.add(self._createItem("Red", Color(255, 50, 0)))
         subMenu.add(self._createItem("Blue", Color(102, 153, 255)))
         subMenu.add(self._createItem("Green", Color(0, 204, 51)))
         subMenu.add(self._createItem("Orange", Color(255, 204, 51)))
         subMenu.add(self._createItem("None", None))
+        subMenu.add(swing.JSeparator())
+        save = swing.JMenuItem("Save now", actionPerformed=self.saveSettings)
+        save.setFont(save.getFont().deriveFont(Font.ITALIC))
+        subMenu.add(save)
         menu.add(subMenu)
         return menu
 
     @fix_exception
-    def _highlight_tab(self, event, colour, style):
-        idx = self._repeater.getSelectedIndex()
+    def _highlight_tab(self, event, colour, style, idx=None):
+        if not idx:
+            idx = self._repeater.getSelectedIndex()
+        # print("Setting tab {} to {}, {}".format(self._repeater.getSelectedIndex(), colour, style))
         self._repeater.setBackgroundAt(idx, colour)
         tab = self._repeater.getTabComponentAt(idx)
         tabLabel = tab.getComponent(0)
-        font = tabLabel.getFont()
-        tabLabel.setFont(Font(font.getName(), style, font.getSize()))
+        tabLabel.setFont(tabLabel.getFont().deriveFont(style))
         tabName = self._repeater.getTitleAt(idx)
         self._tabs[tab] = [colour, style]
+
+
+class ConfigStoreHttpService(IHttpService):
+
+    def __init__(self, url):
+        u = urlparse(url)
+        if u.scheme in ("http", "https"):
+            self._protocol = u.scheme
+        else:
+            raise ValueError("Invalid protocol {}".format(u.scheme))
+
+        if not u.hostname:
+            raise ValueError("Invalid host {}".format(u.hostname))
+        self._host = u.hostname
+
+        self._port = u.port if u.port else 443 if u.scheme.lower() == 'https' else 80
+
+        if not len(u.path) > 1:
+            raise ValueError("Invalid path {}".format(u.path))
+        self._path = u.path
+
+    def getHost(self):
+        return self._host
+
+    def getPort(self):
+        return self._port
+
+    def getProtocol(self):
+        return self._protocol
+
+    def getPath(self):
+        return self._path
+
+
+class ConfigStoreRequestResponse(IHttpRequestResponse):
+
+    def __init__(self, url, value):
+        self._service = ConfigStoreHttpService(url)
+        self._request = (
+            "GET {} HTTP/1.1\r\nHost: {}\r\n\r\n" 
+            "You can ignore this item in the site map. It was created by the Tab Highlighter extension because "
+            "the Burp extensions API is missing the capability to save project-specific settings.\r\n\r\n"
+            "Shamelessly stolen from the Response Clusterer extension - huge thanks! :)"
+        ).format(self._service.getPath(), self._service.getHost())
+        self.setValue(value)
+
+    def setValue(self, value):
+        self._response = (
+            "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n" + value
+        )
+
+    def getComment(self):
+        return ''
+
+    def getHighlight(self):
+        return ''
+
+    def getHttpService(self):
+        return self._service
+
+    def getRequest(self):
+        return self._request
+
+    def getResponse(self):
+        return self._response
+
+    def setComment(self, comment):
+        pass
+
+    def setHighlight(self, color):
+        pass
+
+    def setHttpService(self, httpService):
+        pass
+
+    def setRequest(self, message):
+        pass
+
+    def setResponse(self, message):
+        pass
 
